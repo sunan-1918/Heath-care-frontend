d7fab4455657de4697d903376a0b1ec55839afaa
Sunan13208 <sunanrabbi1918@gmail.com>
2024-05-06 22:05:00 +0600

    first commit

---
+++ src/lib/Providers/Providers.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "@mui/material";
+import React from "react";
+import { theme } from "../theme/theme";
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <ThemeProvider theme={theme}>
+            {children}
+        </ThemeProvider>
+    );
+};
+
+export default Providers;
---
+++ src/lib/theme/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material/styles';
+
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#1586FD",
+        },
+        secondary: {
+            main: "#666f73",
+        },
+    },
+});
