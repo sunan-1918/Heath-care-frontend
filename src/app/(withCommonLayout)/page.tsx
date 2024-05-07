e564786f1995f56b12e43c5c1f52dd8547292dca
Sunan13208 <sunanrabbi1918@gmail.com>
2024-05-07 14:51:27 +0600

    top rated doctor

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import Herosection from "@/components/UI/Homepage/HeroSection/Herosection";
 import Specialist from "@/components/UI/Homepage/Specialist/Specialist";
+import TopRatedDoctor from "@/components/UI/Homepage/TopRatedDoctor/TopRatedDoctor";
 
 export default function HomePage() {
   return (
     <div>
       <Herosection />
       <Specialist />
+      <TopRatedDoctor />
     </div>
   )
 }
